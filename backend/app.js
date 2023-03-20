f4720dbb726a9a9f1834da1e45499785a136a431
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-20 12:21:13 +0100

    ändrade db endpoint till alen-gusinac

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/alen-gusinac');
 
   console.log('Connected to database.');
 }
