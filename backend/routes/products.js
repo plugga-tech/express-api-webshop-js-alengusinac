89b7b924b65ebeca94b7be6ca33136baf1dc1fdd
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-21 11:12:07 +0100

    added all products routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,40 @@
 var express = require('express');
 var router = express.Router();
-const UserModel = require('../models/User');
+const ProductModel = require('../models/Product');
 
-router.get('/', (req, res) => {});
+router.get('/', async (req, res) => {
+  try {
+    const products = await ProductModel.find();
+    if (products.length > 0) {
+      res.status(200).json(products);
+    } else {
+      res.status(404).send("Couldn't find any products.");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    if (product) {
+      res.status(200).json(product);
+    }
+  } catch {
+    res.status(404).send('Product not found.');
+  }
+});
+
+router.post('/add', async (req, res) => {
+  try {
+    const product = await ProductModel.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
+  }
+});
 
 module.exports = router;
