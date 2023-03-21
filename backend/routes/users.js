995735a733bee2cd17247bb2f506e846bb536388
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-21 11:11:43 +0100

    added try/catch to router calls

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,35 +4,52 @@ const UserModel = require('../models/User');
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
-  const users = await UserModel.find();
-  if (users.length > 0) {
-    res.status(200).json(users);
-  } else {
-    res.status(404).send('Kunde inte hitta några användare.');
+  try {
+    const users = await UserModel.find();
+    if (users.length > 0) {
+      res.status(200).json(users);
+    } else {
+      res.status(404).send('Kunde inte hitta några användare.');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
   }
 });
 
 router.post('/', async (req, res) => {
-  const user = await UserModel.find({ _id: req.body.id });
-  if (user.length > 0) {
-    res.status(200).json(user);
-  } else {
-    res.status(404).send('Kunde inte hitta någon användare.');
+  try {
+    const user = await UserModel.find({ _id: req.body.id });
+    if (user.length > 0) {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(404).send('User not found.');
   }
 });
 
 router.post('/add', async (req, res) => {
-  const user = await UserModel.create(req.body);
-  res.status(201).json(user);
+  try {
+    const user = await UserModel.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
+  }
 });
 
 router.post('/login', async (req, res) => {
-  const user = await UserModel.findOne(req.body);
-  console.log(user);
-  if (user) {
-    res.status(200).json(user);
-  } else {
-    res.status(404).send('E-mail och lösenord matchar inte.');
+  try {
+    const user = await UserModel.findOne(req.body);
+    console.log(user);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).send('E-mail och lösenord matchar inte.');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
   }
 });
 
