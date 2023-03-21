9699bce36f9e769ffa2b508261d71831a0c18f1f
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-21 13:38:26 +0100

    added order routes

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var router = express.Router();
-const UserModel = require('../models/User');
+const OrderModel = require('../models/Order');
+
+router.post('/add', async (req, res) => {
+  try {
+    const order = await OrderModel.create(req.body);
+    res.status(201).json(order);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
+  }
+});
+
+router.get('/all', async (req, res) => {
+  try {
+    const orders = await OrderModel.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
+  }
+});
 
 module.exports = router;
