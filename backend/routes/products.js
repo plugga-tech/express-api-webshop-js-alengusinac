5491f1509b2bc6118049f8b156db48a49d27994a
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-22 12:54:17 +0100

    bug fix: catch error on post

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -21,9 +21,12 @@ router.get('/:id', async (req, res) => {
     const product = await ProductModel.findById(req.params.id);
     if (product) {
       res.status(200).json(product);
+    } else {
+      res.status(404).send('Could not find product.');
     }
-  } catch {
-    res.status(404).send('Could not find product.');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error.');
   }
 });
 
