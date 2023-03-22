9a571abd863bc0fc36e2ab7519d1f45dc634d5b4
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-22 12:54:55 +0100

    added more details to model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('user', UserSchema);
