1ec0a029eab076df9b3d09d0c35516d1231417d5
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-23 13:18:06 +0100

    added epikey access to orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -12,13 +12,35 @@ router.post('/add', async (req, res) => {
   }
 });
 
-router.get('/all', async (req, res) => {
+router.get('/all/:token', async (req, res) => {
   try {
     const orders = await OrderModel.find();
-    if (orders.length > 0) {
-      res.status(200).json(orders);
+    if (req.params.token === process.env.APY_KEY) {
+      if (orders.length > 0) {
+        res.status(200).json(orders);
+      } else {
+        res.status(404).send('Could not find orders.');
+      }
     } else {
-      res.status(204).send('Could not find orders.');
+      res.status(401).json({ message: 'Not authorised!' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+router.post('/user', async (req, res) => {
+  try {
+    const order = await OrderModel.find({ user: req.body.user });
+    if (req.body.token === process.env.APY_KEY) {
+      if (order.length > 0) {
+        res.status(200).json(order);
+      } else {
+        res.status(404).send('Could not find any orders.');
+      }
+    } else {
+      res.status(401).json({ message: 'Not authorised!' });
     }
   } catch (err) {
     console.error(err);
