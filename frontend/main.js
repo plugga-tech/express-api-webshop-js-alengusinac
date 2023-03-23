f2b4a9dc1c264c5b4a700f5c5de7026a8a011f1b
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-23 16:53:42 +0100

    added category and cart functions

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,7 @@
-import './style.css';
+import './style.scss';
+
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+console.log(cart);
 
 async function checkLogin() {
   const user = { id: localStorage.getItem('user') };
@@ -45,7 +48,7 @@ function renderLogin(userLoggedIn, username) {
     <input id="create-password" type="text" placeholder="Lösenord"><br>
     <button id="create-btn">Logga in</button>
     `;
-    addEventListeners();
+    addLoginEventListeners();
   }
 }
 
@@ -112,11 +115,114 @@ async function createUser() {
   }
 }
 
-function addEventListeners() {
+function addLoginEventListeners() {
   const loginBtn = document.querySelector('#login-btn');
   loginBtn.addEventListener('click', login);
+
   const createUserBtn = document.querySelector('#create-btn');
   createUserBtn.addEventListener('click', createUser);
 }
 
+async function renderCategories() {
+  const categoriesContainer = document.querySelector('#categories');
+  const categories = await getCategories();
+
+  categoriesContainer.innerHTML =
+    '<button id="show-all-products">Alla produkter</button>';
+
+  categories.map((category) => {
+    categoriesContainer.innerHTML += `
+    <button class="category" id="${category._id}">${category.name}</button>
+    `;
+  });
+  addCategoryEventListeners();
+}
+
+async function getCategories() {
+  const categories = await fetch('http://localhost:3000/api/categories')
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+
+  return categories;
+}
+
+function addCategoryEventListeners() {
+  const showAll = document.querySelector('#show-all-products');
+  showAll.addEventListener('click', () => {
+    renderProducts();
+  });
+
+  const buttons = document.querySelectorAll('.category');
+  buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const id = e.currentTarget.id;
+      const endpoint = `/category/${id}`;
+      renderProducts(endpoint);
+    });
+  });
+}
+
+async function renderProducts(category = '') {
+  const productsContainer = document.querySelector('#products');
+  const products = await getProducts(category);
+
+  productsContainer.innerHTML = '';
+
+  products.map((product) => {
+    productsContainer.innerHTML += `
+    <div class="product" id="${product._id}">
+      <img src="/img/product.webp">
+      <h4>${product.name}</h4>
+      <p>${product.price}</p>
+      <button class="add-to-cart">+</button>
+    </div>
+    `;
+  });
+  const buttons = document.querySelectorAll('.add-to-cart');
+  buttons.forEach((button) => {
+    button.addEventListener('click', addToCart);
+  });
+}
+
+async function getProducts(category = '') {
+  const products = await fetch('http://localhost:3000/api/products' + category)
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+
+  return products;
+}
+
+function addToCart(e) {
+  console.log(e.currentTarget.parentElement.querySelector('h4').innerHTML);
+  const product = e.currentTarget.parentElement;
+  const cartItem = {
+    id: product.id,
+    name: product.querySelector('h4').innerHTML,
+    price: product.querySelector('p').innerHTML,
+    amount: 1,
+  };
+  cart.push(cartItem);
+  localStorage.setItem('cart', JSON.stringify(cart));
+  renderCart();
+}
+
+function renderCart() {
+  const cartContainer = document.querySelector('#cart');
+  if (cart.length > 0) {
+    cartContainer.innerHTML = '';
+    cart.forEach((item) => {
+      cartContainer.innerHTML += `
+      <h4>${item.name}</h4>
+      <p>Pris: ${item.price}kr</p>
+      <p>Antal: ${item.amount}</p>
+      `;
+    });
+  } else {
+    cartContainer.innerHTML = 'Kundvagnen är tom.';
+  }
+}
+
 checkLogin();
+renderCart();
+renderCategories();
+renderProducts();
